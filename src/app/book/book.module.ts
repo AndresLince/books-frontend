7ac69a9be198190b3f880c0e21d53b405628db46
AndresLince <lynxdarmatron@gmail.com>
2022-08-23 05:59:33 -0500

    00031 - Improve app header component styles

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -5,14 +5,15 @@ import { BookSearchComponent } from './book-search/book-search.component';
 import { BookRoutingModule } from './book.routing';
 import { BookComponent } from './book/book.component';
 import { ReadedBooksComponent } from './readed-books/readed-books.component';
-
+import { HeaderComponent } from '../shared/header/header.component';
 
 
 @NgModule({
   declarations: [
     BookSearchComponent,
     BookComponent,
-    ReadedBooksComponent
+    ReadedBooksComponent,
+    HeaderComponent
   ],
   imports: [
     BookRoutingModule,
